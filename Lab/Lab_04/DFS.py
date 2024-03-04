a94a96dca8ed0cbe3df008934d8695cc8dc13b70
Adil Feroze <115704572+Ash4477@users.noreply.github.com>
2024-03-04 14:43:00 +0500

    add different function for printing path in  DFS for lab04

--- Lab/Lab_04/DFS.py
+++ Lab/Lab_04/DFS.py
@@ -2,6 +2,15 @@ import queue
 
 # Functions
 
+def printPath(parentArray, goalNode) :
+    nodeArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+    i = goalNode
+    while (parentArray[i] != -1):
+        print(nodeArray[i], " <- ", end = "")
+        i = parentArray[i]
+    print('A <- ')
+
+
 def depthFirstTraversal(graph) :
     myStack = queue.LifoQueue()
 
@@ -28,6 +37,26 @@ def depthFirstTraversal(graph) :
         i = parentArray[i]
     print('A <- ')
 
+def depthFirstSearch(graph, goalNode) :
+    myStack = queue.LifoQueue()
+
+    visArray = [False, False, False, False, False, False, False]
+    parentArray = [-1, -1, -1, -1, -1, -1, -1]
+    startNode = 0
+
+    myStack.put(startNode)
+
+    while (not myStack.empty() and startNode != goalNode):
+        if (visArray[startNode] == False):
+            for j in range(len(graph[0])):
+                if (graph[startNode][j] == True):
+                    myStack.put(j)
+                    parentArray[j] = startNode
+        
+        visArray[startNode] = True
+        startNode = myStack.get()
+    printPath(parentArray, goalNode)
+
 
 # Main Program         
 graph = [
@@ -41,7 +70,7 @@ graph = [
 ]
 
 
-
-depthFirstTraversal(graph)
+goalNode = 5
+depthFirstSearch(graph, goalNode)
 
 
